f596edf953f6f236cac45ad5fd2de1c7ceabc1e6
Sicelo Mduna <sicelomdunacareer@gmail.com>
2020-12-04 20:07:28 +0200

    changes to add useful variable names

--- Task 5.py
+++ Task 5.py
@@ -1,16 +1,16 @@
 from math import sqrt
 
 
-def area_of_triangle(a,b,c):
+def area_of_triangle(side1,side2,side3):
     '''
     Calculate the area of a triangle given only 3 sides, a,b,c
     '''
-    S = (a + b + c) / 2
-    A = (((S - a) * (S - b) * (S - c)) * S)
-    C = sqrt(A)
+    Semiperimeter = (side1 + side2 + side3) / 2
+    Area = (((Semiperimeter - side1) * (Semiperimeter - side2) * (Semiperimeter - side3)) * Semiperimeter)
+    Final_answer = sqrt(Area)
 
 
-    return (C)
+    return (Final_answer)
 
 
 
--- Task 7.py
+++ Task 7.py
@@ -1,28 +1,28 @@
 NumList = []
 
-def Celsius_to_faranheit(C):
+def Celsius_to_faranheit(Temperature_in_celcius):
     '''
     This function converts Celsius into Faranheit abd appends my list with the new value
     '''
-    F = (((9 / 5) * C) + 32)
-    return (F)
+    Temperature_in_faranheit = (((9 / 5) * Temperature_in_celcius) + 32)
+    return (Temperature_in_faranheit)
 
-def Faranheit_to_celsius(F):
+def Faranheit_to_celsius(Temperature_in_faranheit):
     '''
     This function converts Faranheit into Celsius and appends my listw with the new value
     '''
-    C = ((F - 32) * (5 / 9))
-    return (C)
+    Temperature_in_celcius = ((Temperature_in_faranheit - 32) * (5 / 9))
+    return (Temperature_in_celcius)
 
 user_answer_1 = input("Do you wish to convert Celsius to Faranheit(y/n) ?: ")
 if((user_answer_1 == 'y') or (user_answer_1 == 'Y')):
-    C = int(input("Enter the Temperature in Celsius : "))
-    NumList.append(C)
-    print(Celsius_to_faranheit(C))
+    Temperature_in_celcius = int(input("Enter the Temperature in Celsius : "))
+    NumList.append(Temperature_in_celcius)
+    print(Celsius_to_faranheit(Temperature_in_celcius))
 
 else:
     user_answer_2 = input("Do you wish to convert Faranheit to Celsius(y/n) ?: ")
     if((user_answer_2 == 'y') or (user_answer_2 == 'Y')):
-        F = int(input("Enter the Temperature in Faranheit : "))
-        NumList.append(F)
-        print(Faranheit_to_celsius(F))
+        Temperature_in_faranheit = int(input("Enter the Temperature in Faranheit : "))
+        NumList.append(Temperature_in_faranheit)
+        print(Faranheit_to_celsius(Temperature_in_faranheit))
