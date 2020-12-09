d109908b0cc2e5b7597981c6135e416febf2f8fb
Sicelo Mduna <sicelomdunacareer@gmail.com>
2020-12-09 15:59:26 +0200

    added comments to my code to explain the logic

--- Task 1.py
+++ Task 1.py
@@ -2,10 +2,7 @@ x = 0
 y = 1
 print(x)
 print(y)
-'''
-Create an increment operand using the += operands
-'''
-x +=  3
-y +=  x
+x +=  3            # x += 3, is the same as x = x + 3, where the current value of x is 0, therefore x = (0 +3) = 3
+y +=  x            # y += x, is the same as y = y + x, where the current value of y is 1 and x is 3, therefore y = (1+3) = 4
 print(x)
 print(y)
