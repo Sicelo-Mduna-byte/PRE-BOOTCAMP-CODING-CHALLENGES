888983e3d1a1e5cf4cadfd6638eb5f99af6c33d5
Sicelo Mduna <sicelomdunacareer@gmail.com>
2020-12-09 20:28:03 +0200

    re-wriiten as per review instructions
    renamed my function and variables useful names

--- Task 4.py
+++ Task 4.py
@@ -1,18 +1,12 @@
+def determine_the_status_of_the_input(first_number,second_number):                 # check if either of the numbers is 3 AND the sum of the numbers contains a 3 then return true. Otherwise return false
 
-def my_function(x,y):
-
-    sum = str(x+y)
-    """
-    Function that takes 2 numbers as input, if either of the numbers is 3 AND the sum of the numbers 
-    contains a 3 then return true. Otherwise return False
-
-    """
+    sum_of_the_two_numbers = str(first_number+second_number)
     
-    if((x or y == 3) and ("3" in (sum))):
+    if((first_number or second_number == 3) and ("3" in (sum_of_the_two_numbers))):
 
         return True
     
     else:
         return False
 
-print(my_function(3,15))
+print(determine_the_status_of_the_input(3,15))
