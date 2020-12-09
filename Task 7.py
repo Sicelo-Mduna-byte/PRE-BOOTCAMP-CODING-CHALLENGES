c4ed574a628bba909ab2974c50a0a62bdaec81ab
Sicelo Mduna <sicelomdunacareer@gmail.com>
2020-12-09 16:57:23 +0200

    changed my variable names, and the position of my list array

--- Task 7.py
+++ Task 7.py
@@ -1,28 +1,25 @@
-NumList = []
-
-def Celsius_to_faranheit(Temperature_in_celcius):
-    '''
-    This function converts Celsius into Faranheit abd appends my list with the new value
-    '''
-    Temperature_in_faranheit = (((9 / 5) * Temperature_in_celcius) + 32)
+def convert_celsius_to_faranheit(temperature_in_celcius):
+    Temperature_in_faranheit = (((9 / 5) * temperature_in_celcius) + 32)
     return (Temperature_in_faranheit)
 
-def Faranheit_to_celsius(Temperature_in_faranheit):
-    '''
-    This function converts Faranheit into Celsius and appends my listw with the new value
-    '''
-    Temperature_in_celcius = ((Temperature_in_faranheit - 32) * (5 / 9))
-    return (Temperature_in_celcius)
+def convert_faranheit_to_celsius(temperature_in_faranheit):
+    temperature_in_celcius = ((temperature_in_faranheit - 32) * (5 / 9))
+    return (temperature_in_celcius)
 
-user_answer_1 = input("Do you wish to convert Celsius to Faranheit(y/n) ?: ")
-if((user_answer_1 == 'y') or (user_answer_1 == 'Y')):
-    Temperature_in_celcius = int(input("Enter the Temperature in Celsius : "))
-    NumList.append(Temperature_in_celcius)
-    print(Celsius_to_faranheit(Temperature_in_celcius))
+
+
+the_entered_temperature = []  
+
+user_answer_for_celcius_to_faranheit_conversion  = input("Do you wish to convert Celsius to Faranheit(y/n) ?: ")
+if((user_answer_for_celcius_to_faranheit_conversion == 'y') or (user_answer_for_celcius_to_faranheit_conversion == 'Y')):
+    temperature_in_celcius = int(input("Enter the Temperature in Celsius : "))
+    the_entered_temperature.append(temperature_in_celcius)
+    print(temperature_in_celcius, "degrees Celsius converted to degrees Faranheit is:", convert_celsius_to_faranheit(temperature_in_celcius))
 
 else:
-    user_answer_2 = input("Do you wish to convert Faranheit to Celsius(y/n) ?: ")
-    if((user_answer_2 == 'y') or (user_answer_2 == 'Y')):
-        Temperature_in_faranheit = int(input("Enter the Temperature in Faranheit : "))
-        NumList.append(Temperature_in_faranheit)
-        print(Faranheit_to_celsius(Temperature_in_faranheit))
+    user_answer_for_faranheit_to_celcius_conversion = input("Do you wish to convert Faranheit to Celsius(y/n) ?: ")
+    if((user_answer_for_faranheit_to_celcius_conversion == 'y') or (user_answer_for_faranheit_to_celcius_conversion == 'Y')):
+        temperature_in_faranheit = int(input("Enter the Temperature in Faranheit : "))
+        the_entered_temperature.append(temperature_in_faranheit)
+        print(temperature_in_faranheit, "degrees Faranheit converted to degrees Celsius is: %.2f" % (convert_faranheit_to_celsius(temperature_in_faranheit)))
+                                                                                                  
