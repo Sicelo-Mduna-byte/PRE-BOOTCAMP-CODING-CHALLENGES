fba066bc41ef1adf339e21c585b2f8c17ab516e5
Sicelo Mduna <sicelomdunacareer@gmail.com>
2020-12-10 17:01:30 +0200

    removed all unnecessary spaces from the code

--- Task 3.py
+++ Task 3.py
@@ -1,10 +1,7 @@
 def sum_of_arguments_is_65_decider(first_number,second_number):
-
     if((first_number == 65 or second_number == 65) or ((first_number + second_number) == 65)):
-
         return True
-    
     else:
         return False
-
+    
 print(sum_of_arguments_is_65_decider(55,10))
