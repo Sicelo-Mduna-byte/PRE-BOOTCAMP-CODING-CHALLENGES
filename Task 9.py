3395a94ce5baa0888ce22848461869c73c12b46a
Sicelo Mduna <sicelomdunacareer@gmail.com>
2020-12-10 19:02:45 +0200

    removed all unnecessary code, and spaces

--- Task 9.py
+++ Task 9.py
@@ -7,12 +7,9 @@ def sum_of_multiples_of_3_and_10_below_1000():
     
     for i in range(5, 1000, 5):                                                                                                    
        list_contaning_multiples_of_five.append(i)
+       
     sum_of_multiples_of_list_contaning_multiples_of_five = (sum(list_contaning_multiples_of_five))
-    
-
-
-
     sum_of_multiples_of_3_and_10_below_1000 = sum_of_multiples_of_list_contaning_multiples_of_three + sum_of_multiples_of_list_contaning_multiples_of_five
     return sum_of_multiples_of_3_and_10_below_1000
 
-print("The sum of all the multiples of 3 and 5 below 1000 is: %d " % (sum_of_multiples_of_3_and_10_below_1000()))
+print(sum_of_multiples_of_3_and_10_below_1000())
