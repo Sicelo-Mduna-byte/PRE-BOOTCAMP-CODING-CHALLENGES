c08d09f2318f6a3fa9e0164ac668222215552503
Sicelo Mduna <sicelomdunacareer@gmail.com>
2020-12-10 19:00:10 +0200

    removed all unnecessary code

--- Task 6.py
+++ Task 6.py
@@ -7,4 +7,4 @@ def maximum_value_finder(number_1, number_2, number_3):
         current_maximum_value = number_3
     return current_maximum_value                     
 
-print("The maximum number entered is:", maximum_value_finder(20, 30, 20))
+print(maximum_value_finder(20, 30, 20))
