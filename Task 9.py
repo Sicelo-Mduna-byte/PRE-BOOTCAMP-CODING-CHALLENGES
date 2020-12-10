2fd759421b2dab1e092140f4f9cc75f0213c1ddb
Sicelo Mduna <sicelomdunacareer@gmail.com>
2020-12-10 18:15:23 +0200

    fixed output

--- Task 9.py
+++ Task 9.py
@@ -15,4 +15,4 @@ def sum_of_multiples_of_3_and_10_below_1000():
     sum_of_multiples_of_3_and_10_below_1000 = sum_of_multiples_of_list_contaning_multiples_of_three + sum_of_multiples_of_list_contaning_multiples_of_five
     return sum_of_multiples_of_3_and_10_below_1000
 
-print("The sum of all the multiples of 3 and 5 below 16 is: %d " % (sum_of_multiples_of_3_and_10_below_1000()))
+print("The sum of all the multiples of 3 and 5 below 1000 is: %d " % (sum_of_multiples_of_3_and_10_below_1000()))
