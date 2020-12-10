755699c89602b32e8fad82449dde6352a2ef5e07
Sicelo Mduna <sicelomdunacareer@gmail.com>
2020-12-10 16:41:55 +0200

    removed the import statement, and raised area to 0.5 to get its square root

--- Task 5.py
+++ Task 5.py
@@ -1,11 +1,7 @@
-from math import sqrt
-
 def area_of_triangle(side1,side2,side3):                                    # returns the area of a triangle with 3 sides
     semiperimeter = (side1 + side2 + side3) / 2
     area = (((semiperimeter - side1) * (semiperimeter - side2) * (semiperimeter - side3)) * semiperimeter)
-    final_answer = sqrt(area)
-
-
+    final_answer = area ** 0.5
     return (final_answer)
 
 print(area_of_triangle(3,4,5))
