48ee60a584dacc3fd3c082a77200847ecddc2507
Sicelo Mduna <sicelomdunacareer@gmail.com>
2020-12-10 17:11:36 +0200

    removed all unnecessary blank spaces from the code

--- Task 4.py
+++ Task 4.py
@@ -1,11 +1,7 @@
-def determine_the_status_of_the_input(first_number,second_number):                 # check if either of the numbers is 3 AND the sum of the numbers contains a 3 then return true. Otherwise return false
-
+def determine_the_status_of_the_input(first_number,second_number):               
     sum_of_the_two_numbers = str(first_number+second_number)
-    
     if((first_number or second_number == 3) and ("3" in (sum_of_the_two_numbers))):
-
         return True
-    
     else:
         return False
 
