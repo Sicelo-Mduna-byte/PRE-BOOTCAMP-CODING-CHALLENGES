073767af74c7ad9d6ce4edc4972c1e7572bf9948
Sicelo-Mduna-byte <sicelomdunacareer@gmail.com>
2020-12-12 13:07:28 +0200

    updated as requested by rokhuda.tshitimbi@umuzi.org
    Updated the function to work and give the given output

--- Task 9.py
+++ Task 9.py
@@ -1,15 +1,8 @@
 def sum_of_multiples_of_3_and_10_below_1000():
-    list_contaning_multiples_of_three = []
-    list_contaning_multiples_of_five = [] 
-    for i in range(3, 1000, 3):                                                                                                   
-        list_contaning_multiples_of_three.append(i)
-    sum_of_multiples_of_list_contaning_multiples_of_three = sum(list_contaning_multiples_of_three)
-    
-    for i in range(5, 1000, 5):                                                                                                    
-       list_contaning_multiples_of_five.append(i)
-       
-    sum_of_multiples_of_list_contaning_multiples_of_five = sum(list_contaning_multiples_of_five)
-    sum_of_multiples_of_3_and_10_below_1000 = sum_of_multiples_of_list_contaning_multiples_of_three + sum_of_multiples_of_list_contaning_multiples_of_five
-    return sum_of_multiples_of_3_and_10_below_1000
+    answer = 0
+    for i in range(0,1000):
+        if (i % 3 == 0 or i % 5 == 0):
+            answer = answer + i
+    return answer
 
 print(sum_of_multiples_of_3_and_10_below_1000())
