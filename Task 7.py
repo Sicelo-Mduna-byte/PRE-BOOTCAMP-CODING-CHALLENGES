ef3e8e15e38968116da13ae7a1635cbac8f66910
Sicelo-Mduna-byte <sicelomdunacareer@gmail.com>
2020-12-14 13:38:55 +0200

    changes as requested by sheena.oconnell@umuzi.org
    re-written the return statement to be clear, and look as though it is returning a variable instead of a tuple(removed the unnecessary brackets)

--- Task 7.py
+++ Task 7.py
@@ -1,8 +1,7 @@
 def convert_celsius_to_faranheit(temperature_in_celcius):
     temperature_in_faranheit = (((9 / 5) * temperature_in_celcius) + 32)
-    return (temperature_in_faranheit)
+    return temperature_in_faranheit
 
 def convert_faranheit_to_celsius(temperature_in_faranheit):
     temperature_in_celcius = ((temperature_in_faranheit - 32) * (5 / 9))
-    return (temperature_in_celcius)
-
+    return temperature_in_celcius
