64b0c29724332bb6fd37971ccc47ad5adc1053bb
Sicelo-Mduna-byte <sicelomdunacareer@gmail.com>
2020-12-14 13:20:50 +0200

    changes as requested by 71click@gmail.com
    changed the code to include print statements inside the function

--- Task 8.py
+++ Task 8.py
@@ -2,13 +2,12 @@ def converter(number_that_has_to_be_converted):
      hour = int(number_that_has_to_be_converted / 60) 
      minute = int(number_that_has_to_be_converted % 60)           
      if(hour <= 1 and minute <= 1):
-         return (" %d hour %d minute" % (hour, minute))
+         print(" %d hour %d minute" % (hour, minute))
      if(hour <= 1 and minute > 1):
-         return (" %d hour %d minutes" % (hour, minute))
+         print(" %d hour %d minutes" % (hour, minute))
      if(hour > 1 and minute <= 1):
-         return (" %d hours %d minute" % (hour, minute))
+         print(" %d hours %d minute" % (hour, minute))
      if(hour > 1 and minute > 1):
-         return (" %d hours %d minutes" % (hour, minute))
-     return()
+         print(" %d hours %d minutes" % (hour, minute))
 
-print(converter(133))
+converter(133)
