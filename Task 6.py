0d6b0599c853580d1ac70b7951a3c623061cdc55
Sicelo-Mduna-byte <sicelomdunacareer@gmail.com>
2020-12-15 21:57:31 +0200

    changes as requested by 71click@gmail.com

--- Task 6.py
+++ Task 6.py
@@ -3,8 +3,8 @@ def maximum_value_finder(number_1, number_2, number_3):
     current_maximum_value = number_1 
     if(number_2 > number_1):
         current_maximum_value = number_2
-    if(number_3 > number_2):
+    if(number_3 > number_2 > number_1):
         current_maximum_value = number_3
     return current_maximum_value                     
 
-print(maximum_value_finder(20, 30, 20))
+print(maximum_value_finder(30, 10, 20))
