9596d2ca292fb4df35285146cc0e78529d00184e
Sicelo-Mduna-byte <sicelomdunacareer@gmail.com>
2020-12-15 22:07:53 +0200

    changes as requested by 71click@gmail.com

--- Task 10.py
+++ Task 10.py
@@ -1,12 +1,12 @@
 def vowel_finder(string_entered_by_user):
-    string_in_lowercase = []
-    string_in_lowercase.extend(string_entered_by_user.lower())
-    vowels = "aeiou"
-    similar_vowel_list = []
-    for i in string_in_lowercase:
-            if((i in vowels) and (i not in similar_vowel_list)):
-                    similar_vowel_list.append(i)
-    vowels = (", " .join(similar_vowel_list))
+    string = []
+    string.extend(string_entered_by_user)
+    vowels = "aAeEiIoOuU"
+    vowel_list = []
+    for i in string:
+            if(i in vowels):
+                vowel_list.append(i)
+    vowels = (", " .join(vowel_list))
     print("The following vowels are contained in the string: %s" % (vowels))
 
 vowel_finder("Hello Egg")
